d568b94dadbb7fd3d8c3d66f6055b52522e3bf7f
Sébastien Muler <sebastien.muler@mulertech.net>
2025-08-31 16:37:19 +0200

    Enhance Docker container startup process with real-time progress display and error handling

--- templates/symfony/init-user.sql
+++ templates/symfony/init-user.sql
@@ -1,4 +1,6 @@
 -- Create user with network access like in the old working system
 CREATE USER IF NOT EXISTS 'user'@'%' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON db.* TO 'user'@'%';
+-- Grant permission to create databases
+GRANT CREATE ON *.* TO 'user'@'%';
 FLUSH PRIVILEGES;
