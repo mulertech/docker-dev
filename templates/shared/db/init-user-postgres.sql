2318a432cbefd983f9ad1420747eee61467bf31b
Sébastien Muler <sebastien.muler@mulertech.net>
2026-03-03 18:56:35 +0100

    Add script to create test database if it does not exist

--- templates/shared/db/init-user-postgres.sql
+++ templates/shared/db/init-user-postgres.sql
@@ -2,5 +2,9 @@
 -- Note: User and database are already created via environment variables
 -- This script is for additional setup if needed
 
+-- Create test database
+SELECT 'CREATE DATABASE db_test'
+    WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'db_test')\gexec
+
 -- Grant additional privileges if needed
 -- ALTER USER user CREATEDB;
